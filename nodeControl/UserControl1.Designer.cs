fff47601f6192d7cfcd51ad3e465b4cf89fcba15
Simflare <stu23@live.com>
2016-08-25 16:45:50 +0930

    Added Custom Control

--- nodeControl/UserControl1.Designer.cs
+++ nodeControl/UserControl1.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace WindowsFormsApplication2
+﻿namespace nodeControl
 {
-    partial class Form2
+    partial class UserControl1
     {
         /// <summary>
         /// Required designer variable.
@@ -20,39 +20,25 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Component Designer generated code
 
         /// <summary>
-        /// Required method for Designer support - do not modify
+        /// Required method for Designer support - do not modify 
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(284, 262);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
-            // 
-            // Form2
+            // UserControl1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(650, 424);
-            this.Controls.Add(this.richTextBox1);
-            this.Name = "Form2";
-            this.Text = "Form2";
+            this.Name = "UserControl1";            
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
